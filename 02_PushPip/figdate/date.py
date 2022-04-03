2aafca2164413ca91035b2025abd5980a57c8408
ozerova daria <ozerova.daria2016@mail.ru>
2022-04-03 20:56:13 +0300

    remove redundant commits

--- 02_PushPip/figdate/date.py
+++
@@ -1,6 +0,0 @@
-import pyfiglet
-import time
-
-def date(fmt='%Y %d %b, %A', font='graceful'):
-    return pyfiglet.figlet_format(time.strftime(fmt), font=font)
-
--- 02_PushPip/figdate_wrapper.py
+++
@@ -1,9 +0,0 @@
-import venv
-from tempfile import TemporaryDirectory
-from subprocess import run
-from sys import argv
-
-with TemporaryDirectory() as tmpDir:
-    venv.create(tmpDir, with_pip=True)
-    run(f'{tmpDir}/bin/pip install pyfiglet --quiet --disable-pip-version-check'.split())
-    run(['python3', '-m', 'figdate', *(argv[1:])])
